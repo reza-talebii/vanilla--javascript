c91c40c56eb86963b66184fa8954e9cabe5d22c3
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-01-15 13:11:05 +0330

    end javascript

--- script/app.js
+++ script/app.js
@@ -1,7 +1,66 @@
 const hands = document.querySelectorAll(".hand");
+const btnReload = document.querySelector("#btn-reload");
+const resultTitle = document.querySelector("#result-title");
+const handsContainer = document.querySelector(".hands");
+const resultContainer = document.querySelector(".result");
+
+let listChoose = ["rock", "paper", "scissors"];
+//PLAYER CHOOSE
+let user = "";
+//USER POINT
+let score = 0;
 
 hands.forEach((hand) => {
   hand.addEventListener("click", (e) => {
-    console.log(e.target);
+    //SELECT COMPUTER
+    let computer = listChoose[Math.trunc(Math.random() * listChoose.length)];
+    user = e.currentTarget.id;
+    hideHandsContent();
+    showResultContent(computer);
+    checkWinner(computer);
   });
 });
+
+const hideHandsContent = () => handsContainer.classList.add("hide");
+
+//SHOW HANDS CHOOSE
+const showResultContent = (computer) => {
+  resultContainer.classList.remove("hide");
+
+  //USER CHOOSE
+  const userChoose = document.querySelector("#user-img-result");
+  userChoose.setAttribute("src", `images/${user}.png`);
+  //COMPUTER CHOOSE
+  const computerChoose = document.querySelector("#computer-img-result");
+  computerChoose.setAttribute("src", `images/${computer}.png`);
+};
+
+const checkWinner = (computer) => {
+  if (user == "rock" && computer == "scissors") {
+    messagePlayerWin("win");
+  } else if (user == "paper" && computer == "rock") {
+    messagePlayerWin("win");
+  } else if (user == "scissors" && computer == "paper") {
+    messagePlayerWin("win");
+  } else if (user == computer) {
+    resultTitle.innerHTML = "DRAW";
+  } else {
+    resultTitle.innerHTML = "YOU ARE LOSE";
+  }
+};
+
+//UPDATE SCORE
+const updateScore = (score) => {
+  document.querySelector("#point").innerHTML = score;
+};
+
+const messagePlayerWin = () => {
+  updateScore(++score);
+  resultTitle.innerHTML = `YOU ARE WIN`;
+};
+
+const restartGame = () => {
+  handsContainer.classList.remove("hide");
+  resultContainer.classList.add("hide");
+};
+btnReload.addEventListener("click", restartGame);
