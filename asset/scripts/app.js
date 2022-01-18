7276284afcf43b024cebe8e2d7cc2959cd67407b
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-01-18 11:27:47 +0330

    end loadMusic & playMusic functions

--- asset/scripts/app.js
+++ asset/scripts/app.js
@@ -1,8 +1,44 @@
 import { listMusic } from "./list-music.js";
+const indexMusic = 0;
 
-//CONTROLS BUTTONS
+const audio = document.querySelector("#audio");
+//*****CONTROLS BUTTONS*******
 const playBtn = document.querySelector(".play"),
   prevBtn = document.querySelector("#prev"),
   nextBtn = document.querySelector("#next"),
   repeatListBtn = document.querySelector("#repeat-plist"),
   moreMusicBtn = document.querySelector("#more-music");
+
+//*******FUNCTIONS*******
+const loadMusic = (index) => {
+  const music = listMusic[index];
+  const coverMusic = document.querySelector("#music-cove"),
+    musicName = document.querySelector("#music-name"),
+    artistName = document.querySelector("#artist-name");
+  //MUSIC COVER
+  coverMusic.src = `asset/images/${music.image}`;
+  // MUSIC NAME
+  musicName.innerHTML = music.name;
+  // MUSIC ARTIST NAME
+  artistName.innerHTML = music.artist;
+  // MUSIC ADDRESS
+  audio.src = `asset/audios/${music.src}`;
+};
+
+const playMusic = () => {
+  if (audio.paused) {
+    audio.play();
+    playBtn.innerHTML = "pause";
+  } else {
+    audio.pause();
+    playBtn.innerHTML = "play_arrow";
+  }
+};
+
+const showListMusic = ()=>{}
+//*******EVENT LISTENER*******
+window.addEventListener("load", () => loadMusic(indexMusic));
+moreMusicBtn.addEventListener("click",showListMusic);
+// prevBtn.addEventListener("click",prevMusic)
+// nextBtn.addEventListener("click",nextMusic)
+playBtn.addEventListener("click", playMusic);
--- asset/scripts/list-music.js
+++ asset/scripts/list-music.js
@@ -2,19 +2,19 @@ const listMusic = [
   {
     name: "Mahaal",
     artist: "Shadmehr Aghili",
-    scr: "Shadmehr Aghili - Mahaal.mp3",
+    src: "Shadmehr Aghili - Mahaal.mp3",
     image: "Shadmehr Aghili - Mahaal.jpg",
   },
   {
     name: "khafan",
     artist: "ho3in",
-    scr: "Ho3ein - Khafan.mp3",
+    src: "Ho3ein - Khafan.mp3",
     image: "Ho3ein - Khafan.jpg",
   },
   {
     name: "Behet Ghol Midam",
     artist: "Mohsen Yeganeh ",
-    scr: "Mohsen Yeganeh - Behet Ghol Midam.mp3",
+    src: "Mohsen Yeganeh - Behet Ghol Midam.mp3",
     image: "Mohsen Yeganeh - Behet Ghol Midam.jpg",
   },
 ];
