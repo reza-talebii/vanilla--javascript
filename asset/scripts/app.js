b18a831a1012f6cacd64acda2f6b40b19e45f4a0
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-01-18 15:43:37 +0330

    delete list music file
    for module

--- asset/scripts/app.js
+++ asset/scripts/app.js
@@ -1,4 +1,31 @@
-import { listMusic } from "./list-music.js";
+const listMusic = [
+  {
+    id: Math.random() * 1000,
+    name: "Mahaal",
+    artist: "Shadmehr Aghili",
+    src: "Shadmehr Aghili - Mahaal.mp3",
+    image: "Shadmehr Aghili - Mahaal.jpg",
+    time: { minute: 4, seconde: 15 },
+  },
+  {
+    id: Math.random() * 1000,
+    name: "khafan",
+    artist: "ho3in",
+    src: "Ho3ein - Khafan.mp3",
+    image: "Ho3ein - Khafan.jpg",
+    time: { minute: 4, seconde: 32 },
+  },
+  {
+    id: Math.random() * 1000,
+    name: "Behet Ghol Midam",
+    artist: "Mohsen Yeganeh ",
+    src: "Mohsen Yeganeh - Behet Ghol Midam.mp3",
+    image: "Mohsen Yeganeh - Behet Ghol Midam.jpg",
+    time: { minute: 3, seconde: 37 },
+  },
+];
+// import { listMusic } from "./list-music.js";
+
 let indexMusic = localStorage.getItem("index") || 0;
 const audio = document.querySelector("#audio");
 
