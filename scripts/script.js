bab1fd2bb0ceeaf4da150f8c9c54782ed84f308a
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-01-20 09:40:34 +0330

    add file scripts & change icon weather

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+// const apiKey = "bc32333c0d19013bb68a06c24eb3e01d";
+
+// const d = async () => {
+//   const res = fetch(
+//     `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}`
+//   );
+//   const data = (await res).json();
+//   return data;
+// };
+
+// console.log(d());
