5e917aae285b00b4fca674100c86fba8f6a33fba
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-01-24 11:26:16 +0330

    add function select tab bar

--- script/budget.js
+++ script/budget.js
@@ -3,6 +3,7 @@ const incomeContainer = document.querySelector("#income");
 const expensesContainer = document.querySelector("#expense");
 const allContainer = document.querySelector("#all");
 
+//show select container list
 const showContainer = (type) => {
   incomeContainer.classList.add("hide");
   expensesContainer.classList.add("hide");
@@ -10,15 +11,18 @@ const showContainer = (type) => {
   type.classList.remove("hide");
 };
 
+//add active class to selected tab bar
 const activeTab_bar = (tabClicked) => {
   [...tab_bar.children].forEach((tab) => tab.classList.remove("active"));
   tabClicked.classList.add("active");
 };
 
-tab_bar.addEventListener("click", (e) => {
+const selectTab_bar = (e) => {
   const tabClicked = e.target.innerText.toLowerCase();
   const selectContent = document.querySelector(`#${tabClicked}`);
 
   activeTab_bar(e.target);
   showContainer(selectContent);
-});
+};
+
+tab_bar.addEventListener("click", (e) => selectTab_bar(e));
