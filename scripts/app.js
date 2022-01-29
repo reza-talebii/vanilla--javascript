1b4945d9b51f40492b68aca0599746a0ba1e0444
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-01-29 14:20:51 +0330

    complete edit page

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,31 @@
+import homeContainer from "./pages/homePage.js";
 import addRecipeContainer from "./pages/add-recipe.js";
 import editRecipeContainer from "./pages/edit-recipe.js";
 
 const recipeList = JSON.parse(localStorage.getItem("recipe")) || [];
 
-const appContainer = document.querySelector("#app");
-const addRecipeButton = document.querySelector("#add-recipe-button");
+const showRecipe = () => {
+  const recipesContainer = document.querySelector("#recipes-div");
+  recipeList.map((recipe, index) => {
+    const item = document.createElement("a");
+    item.href = "/edit-recipes";
+    item.innerHTML = `
+        <h4 class="list-item">${recipe.name}</h4>
+        <h5 class="list-item__title">
+        You have<span> all </span>
+        the ingredients</h5>
+        `;
+    recipesContainer.append(item);
+    //click for edit item
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      navTo(item.href);
+      getEditData(recipe, index);
+    });
+  });
+};
 
-
-
-
-//page
+//direct to the selected page
 const navTo = (url) => {
   history.pushState(null, null, url);
   router();
@@ -19,7 +35,7 @@ const router = () => {
   const routes = [
     {
       path: "/",
-      view: console.log("home"),
+      view: homeContainer(),
     },
     {
       path: "/add-recipes",
@@ -32,10 +48,45 @@ const router = () => {
   ];
 
   const isMatch = routes.find((route) => route.path == location.pathname);
-  appContainer.innerHTML = isMatch.view;
+  document.querySelector("#app").innerHTML = isMatch.view;
+
+  //add & edit get data
+  if (isMatch.path == "/add-recipes") getAddData();
 };
 
-addRecipeButton.addEventListener("click", (e) => {
+//get data in add recipe container
+const getAddData = () => {
+  const recipeName = document.querySelector("#recipe-name");
+  const recipeDescription = document.querySelector("#recipe-description");
+  const addRecipe = document.querySelector("#add-recipe");
+
+  addRecipe.addEventListener("click", () => {
+    const newRecipe = {
+      name: recipeName.value,
+      description: recipeDescription.value,
+    };
+    saveData(newRecipe);
+  });
+};
+
+//get data in edit recipe container
+const getEditData = (recipe, index) => {
+  console.log(recipe, index);
+};
+
+//sava in local storage and push in recipeList
+const saveData = (newRecipe) => {
+  recipeList.push(newRecipe);
+  localStorage.setItem("recipe", JSON.stringify(recipeList));
+  navTo("/");
+  showRecipe();
+};
+
+router();
+
+document.querySelector("#add-recipe-button").addEventListener("click", (e) => {
   e.preventDefault();
   navTo(e.target.href);
 });
+
+showRecipe();
