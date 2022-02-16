400e635db1f3088a4eade7ddabfa10f149e95310
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-02-16 12:51:46 +0330

    complete slider

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,17 @@
-const menu = document.querySelector(".menu-humbugger__list");
-const menuIcon = document.querySelector("#menu-icon");
-const closeIcon = document.querySelector("#close-icon");
-const operationBtnS = document.querySelectorAll(".operations__btn");
-const openModalBtn = document.querySelectorAll(".open-modal");
-const modalCloseBtn = document.querySelector(".modal__close");
-const overlay = document.querySelector(".overlay");
-const modal = document.querySelector(".modal");
-
-//FUNCTIONS
 const addEventElements = (elements, event, func) => {
   elements.forEach((element) => {
     element.addEventListener(event, func);
   });
 };
 
+///////////////////////////////////////
+// **************MENU************
+const menu = document.querySelector(".menu-humbugger__list");
+const menuIcon = document.querySelector("#menu-icon");
+const closeIcon = document.querySelector("#close-icon");
+const operationBtnS = document.querySelectorAll(".operations__btn");
+
+//FUNCTIONS
 const openMenu = () => menu.classList.add("show-menu");
 const closeMenu = () => menu.classList.remove("show-menu");
 
@@ -37,15 +35,91 @@ const operationShowContent = (e) => {
   }
 };
 
+//event handler
+menuIcon.addEventListener("click", openMenu);
+closeIcon.addEventListener("click", closeMenu);
+addEventElements(operationBtnS, "click", operationShowContent);
+
+///////////////////////////////////////
+// **************MODAL************
+const openModalBtn = document.querySelectorAll(".open-modal");
+const modalCloseBtn = document.querySelector(".modal__close");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
+
+//functions
 const toggleModal = (e) => {
   e.preventDefault();
   overlay.classList.toggle("hide");
   modal.classList.toggle("hide");
 };
 
-//LISTENERS
-menuIcon.addEventListener("click", openMenu);
-closeIcon.addEventListener("click", closeMenu);
-addEventElements(operationBtnS, "click", operationShowContent);
+//event handler
 addEventElements(openModalBtn, "click", toggleModal);
 modalCloseBtn.addEventListener("click", toggleModal);
+
+///////////////////////////////////////
+// **************SLIDER************
+const slider = () => {
+  const slides = document.querySelectorAll(".slide");
+  const dotContainer = document.querySelector(".dots");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = () => {
+    dotContainer.innerHTML = "";
+    slides.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+    const dots = document.querySelectorAll(".dots__dot");
+    addEventElements(dots, "click", dotGoSide);
+  };
+
+  const activeDot = () => {
+    const dots = document.querySelectorAll(".dots__dot");
+
+    dots.forEach((dot) => dot.classList.remove("active"));
+    dots[curSlide].classList.add("active");
+  };
+
+  const dotGoSide = (e) => {
+    curSlide = e.target.dataset.slide;
+    goToSlide(curSlide);
+  };
+
+  const goToSlide = (slide) => {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+    activeDot();
+  };
+
+  const nextSlide = () => {
+    curSlide = maxSlide - 1 == curSlide ? 0 : ++curSlide;
+    goToSlide(curSlide);
+  };
+
+  const prevSlide = () => {
+    curSlide = 0 == curSlide ? maxSlide - 1 : --curSlide;
+    goToSlide(curSlide);
+  };
+
+  const init = () => {
+    createDots();
+    activeDot();
+    goToSlide(curSlide);
+  };
+  init();
+
+  //event handler
+  btnLeft.addEventListener("click", prevSlide);
+  btnRight.addEventListener("click", nextSlide);
+};
+
+slider();
